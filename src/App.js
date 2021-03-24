0d05a63a94cf0ae1d5076f493f320d0acdc79d75
Zach Harrison <zharrison@shaw.ca>
2021-03-24 12:39:34 -0700

    Add Navbar and some sample text to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
+import Navbar from './components/Navbar/Navbar';
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Navbar />
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          provident aut, quam tempore facere delectus ut animi iusto vitae
+          itaque nulla sint consectetur dicta quaerat quo voluptas laboriosam
+          quod pariatur unde. Odio ad, aut, eveniet illo laborum quas architecto
+          vero enim, obcaecati nihil dolor quibusdam quod minus veniam vel
+          excepturi iure soluta accusamus inventore harum quaerat maiores! Nobis
+          doloribus ab obcaecati? Minus facere enim beatae blanditiis saepe
+          voluptas veritatis ut ullam velit pariatur voluptate magni minima
+          officiis aspernatur, nemo autem!
+        </p>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          provident aut, quam tempore facere delectus ut animi iusto vitae
+          itaque nulla sint consectetur dicta quaerat quo voluptas laboriosam
+          quod pariatur unde. Odio ad, aut, eveniet illo laborum quas architecto
+          vero enim, obcaecati nihil dolor quibusdam quod minus veniam vel
+          excepturi iure soluta accusamus inventore harum quaerat maiores! Nobis
+          doloribus ab obcaecati? Minus facere enim beatae blanditiis saepe
+          voluptas veritatis ut ullam velit pariatur voluptate magni minima
+          officiis aspernatur, nemo autem!
+        </p>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          provident aut, quam tempore facere delectus ut animi iusto vitae
+          itaque nulla sint consectetur dicta quaerat quo voluptas laboriosam
+          quod pariatur unde. Odio ad, aut, eveniet illo laborum quas architecto
+          vero enim, obcaecati nihil dolor quibusdam quod minus veniam vel
+          excepturi iure soluta accusamus inventore harum quaerat maiores! Nobis
+          doloribus ab obcaecati? Minus facere enim beatae blanditiis saepe
+          voluptas veritatis ut ullam velit pariatur voluptate magni minima
+          officiis aspernatur, nemo autem!
+        </p>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          provident aut, quam tempore facere delectus ut animi iusto vitae
+          itaque nulla sint consectetur dicta quaerat quo voluptas laboriosam
+          quod pariatur unde. Odio ad, aut, eveniet illo laborum quas architecto
+          vero enim, obcaecati nihil dolor quibusdam quod minus veniam vel
+          excepturi iure soluta accusamus inventore harum quaerat maiores! Nobis
+          doloribus ab obcaecati? Minus facere enim beatae blanditiis saepe
+          voluptas veritatis ut ullam velit pariatur voluptate magni minima
+          officiis aspernatur, nemo autem!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default App;
