92aa3f2e44edbc7882340f3e05dba10182a4acb2
Zach Harrison <zharrison@shaw.ca>
2021-03-24 13:10:07 -0700

    Added state and conditional styles so Navbar will hide on scroll event

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { debounce } from '../../utils/helpers';
 import './Navbar.css';
 
 const Navbar = () => {
-  return <nav className='navbar'>Navbar</nav>;
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [visible, prevScrollPos, handleScroll]);
+
+  return (
+    <nav className='navbar' style={{ top: visible ? '0' : '-100px' }}>
+      Navbar
+    </nav>
+  );
 };
 
 export default Navbar;
