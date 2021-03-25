6fa898541fef9bc43b9672cc8988ec17c955abf5
Zach Harrison <zharrison@shaw.ca>
2021-03-24 17:11:21 -0700

    Added animation effect to showcase

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,7 +1,70 @@
 import React from 'react';
 import './Showcase.css';
 const Showcase = () => {
-  return <div className='showcase'></div>;
+  return (
+    <div className='showcase'>
+      <div class='dotWrapper dotWrapper-1'>
+        <div class='dot dot-1'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-2'>
+        <div class='dot dot-2'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-3'>
+        <div class='dot dot-3'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-4'>
+        <div class='dot dot-4'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-5'>
+        <div class='dot dot-5'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-6'>
+        <div class='dot dot-6'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-7'>
+        <div class='dot dot-7'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-8'>
+        <div class='dot dot-8'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-9'>
+        <div class='dot dot-9'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-10'>
+        <div class='dot dot-10'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-11'>
+        <div class='dot dot-11'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-12'>
+        <div class='dot dot-12'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-13'>
+        <div class='dot dot-13'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-14'>
+        <div class='dot dot-14'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-15'>
+        <div class='dot dot-15'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-16'>
+        <div class='dot dot-16'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-17'>
+        <div class='dot dot-17'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-18'>
+        <div class='dot dot-18'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-19'>
+        <div class='dot dot-19'></div>
+      </div>
+      <div class='dotWrapper dotWrapper-20'>
+        <div class='dot dot-20'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Showcase;
