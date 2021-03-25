11feb97e5bc88029ab95e26197c5d1ac3c804f9e
Zach Harrison <zharrison@shaw.ca>
2021-03-25 16:45:38 -0700

    Tweaked more styling for about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,21 @@ const About = () => {
     <div id='about' className='container'>
       <FadeIn>
         <div className='about-content'>
-          <h1>About Me</h1>
+          <div className='about-text'>
+            <h1>Hey, I'm Zach</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
+              vitae quia enim voluptatem qui incidunt in nemo magni modi,
+              doloribus optio, dolores voluptates. Autem, soluta! Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Sed dolore laborum
+              tempore exercitationem autem explicabo in, neque est tempora nobis
+              beatae ratione pariatur ducimus numquam expedita ea totam ut
+              excepturi fugiat fugit corporis odit iusto illo! Dolores
+              doloremque delectus reiciendis deleniti quae sapiente dignissimos
+              eius. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium nam voluptatem aspernatur deserunt doloremque totam?
+            </p>
+          </div>
           <img src='/bio-image.jpg' className='bio-image' alt='bio-image' />
         </div>
       </FadeIn>
