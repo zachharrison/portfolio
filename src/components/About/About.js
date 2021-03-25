e6d0c726d38425e1a78a3467765919357979cf23
Zach Harrison <zharrison@shaw.ca>
2021-03-25 13:09:00 -0700

    Added bio-images and started styling the about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,28 +3,13 @@ import './About.css';
 import FadeIn from '../FadeIn/FadeIn';
 
 const About = () => {
-  // const [prevScrollPos, setPrevScrollPos] = useState(0);
-  // const [visible, setVisible] = useState(true);
-
-  // const handleScroll = () => {
-  //   const currentScrollPos = window.pageYOffset;
-
-  //   setVisible(currentScrollPos >= 700);
-
-  //   // setPrevScrollPos(currentScrollPos);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
-
-  //   return () => window.removeEventListener('scroll', handleScroll);
-  //   // console.log(window.pageYOffset);
-  // }, []);
-
   return (
     <div id='about' className='container'>
       <FadeIn>
-        <h1>About Me</h1>
+        <div className='about-content'>
+          <h1>About Me</h1>
+          <img src='/bio-image.jpg' className='bio-image' alt='bio-image' />
+        </div>
       </FadeIn>
     </div>
   );
