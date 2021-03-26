646854eaede850b882e9d77be22d5707b6b2496d
Zach Harrison <zharrison@shaw.ca>
2021-03-26 13:32:52 -0700

    Added portfolio grid

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,10 +1,63 @@
 import React from 'react';
 import './Work.css';
+// <header>
+//   <h1>
+//     Balkan Style <br /> <span>[ Portfolio Gallery ]</span>
+//   </h1>
+// </header>
 
 const Work = () => {
   return (
-    <div id='work' className='container'>
-      Work
+    <div className='grid-container'>
+      <div className='gallery'>
+        <div className='gallery-item'>
+          <img
+            className='gallery-image'
+            src='/tofino.jpg'
+            alt='project-image'
+          />
+        </div>
+
+        <div className='gallery-item'>
+          <img
+            className='gallery-image'
+            src='/tofino.jpg'
+            alt='project-image'
+          />
+        </div>
+
+        <div className='gallery-item'>
+          <img
+            className='gallery-image'
+            src='/tofino.jpg'
+            alt='project-image'
+          />
+        </div>
+
+        <div className='gallery-item'>
+          <img
+            className='gallery-image'
+            src='/tofino.jpg'
+            alt='project-image'
+          />
+        </div>
+
+        <div className='gallery-item'>
+          <img
+            className='gallery-image'
+            src='/tofino.jpg'
+            alt='project-image'
+          />
+        </div>
+
+        <div className='gallery-item'>
+          <img
+            className='gallery-image'
+            src='/tofino.jpg'
+            alt='project-image'
+          />
+        </div>
+      </div>
     </div>
   );
 };
