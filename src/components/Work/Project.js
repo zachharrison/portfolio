89475fbfc66c9e7129b22eb5ab65055b67455a8b
Zach Harrison <zharrison@shaw.ca>
2021-03-27 10:28:16 -0700

    Added learn more button when hovering over project images

--- src/components/Work/Project.js
+++ src/components/Work/Project.js
@@ -3,7 +3,17 @@ import './Work.css';
 
 const Project = ({ image, name, description }) => {
   return (
-    <div className='gallery-item'>
+    <div
+      className='gallery-item'
+      // style={{
+      //   backgroundColor: 'rgba(0,0,0,0.5)',
+      //   backgroundImage: `url(${image})`,
+      //   backgroundRepeat: 'no-repeat',
+      //   backgroundPosition: 'center',
+      //   backgroundSize: 'cover',
+      // }}
+    >
+      <button className='btn-dark'>Learn More</button>
       <img className='gallery-image' src={image} alt={name} />
     </div>
   );
