d32950041d24ddd824e806dc1aa9fddac48d1f5f
Zach Harrison <zharrison@shaw.ca>
2021-03-27 10:38:00 -0700

    Added more hover effects for project component

--- src/components/Work/Project.js
+++ src/components/Work/Project.js
@@ -3,16 +3,7 @@ import './Work.css';
 
 const Project = ({ image, name, description }) => {
   return (
-    <div
-      className='gallery-item'
-      // style={{
-      //   backgroundColor: 'rgba(0,0,0,0.5)',
-      //   backgroundImage: `url(${image})`,
-      //   backgroundRepeat: 'no-repeat',
-      //   backgroundPosition: 'center',
-      //   backgroundSize: 'cover',
-      // }}
-    >
+    <div className='gallery-item'>
       <button className='btn-dark'>Learn More</button>
       <img className='gallery-image' src={image} alt={name} />
     </div>
