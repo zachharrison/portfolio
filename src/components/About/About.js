f5f1d0aa62541bb616aef5e44d7779298ced000f
Zach Harrison <zharrison@shaw.ca>
2021-03-29 13:55:03 -0700

    Added bio text and more mobile styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './About.css';
-import FadeIn from '../FadeIn/FadeIn';
 
 const About = () => {
   return (
@@ -9,19 +8,9 @@ const About = () => {
         <div className='about-text'>
           <h1>Hey, I'm Zach</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
-            vitae quia enim voluptatem qui incidunt in nemo magni modi,
-            doloribus optio, dolores voluptates. Autem, soluta! Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Sed dolore laborum
-            tempore exercitationem autem explicabo in, neque est tempora nobis
-            beatae ratione pariatur ducimus numquam expedita ea totam ut
-            excepturi fugiat fugit corporis odit iusto illo!
-            <br />
-            <br />
-            Dolores doloremque delectus reiciendis deleniti quae sapiente
-            dignissimos eius. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Accusantium nam voluptatem aspernatur deserunt doloremque
-            totam?
+            Web developer located in beautiful Victoria BC. My speciality is
+            building clean and responsive user interfaces with the most up to
+            date tools in the industry.
           </p>
         </div>
         <img src='/bio-image.jpg' className='bio-image' alt='bio-image' />
