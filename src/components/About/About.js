6ab992e607d0bad9057d0eb6ffffa6bebf032bf0
zachharrison <zharrison100@gmail.com>
2024-10-14 15:14:30 -0700

    add pngs for all skill icons

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div id='about' className='container'>
       <div className='profile-content'>
-        <h1 className='profile-header'>Hi, I'm Zach</h1>
+        <h1 className='profile-header'>Hey, I'm Zach</h1>
         <p className='bio-text'>
           I'm a frontend web developer with an eye for detail, and a passion for
           bringing beautiful user interfaces to life.
--- src/components/Skills/icons/VercelIcon.js
+++ src/components/Skills/icons/VercelIcon.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
 const VercelIcon = () => {
-  return (
-    <svg
-      fill='#000'
-      role='img'
-      viewBox='0 0 24 24'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path d='M24 22.525H0l12-21.05 12 21.05z' />
-    </svg>
-  );
+  return <img src='/skill-icons/vercel.png' alt='vercel-logo' />;
 };
 
 export default VercelIcon;
